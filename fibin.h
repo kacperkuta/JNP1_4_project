5004010da9287a58a9f5e21a99a2754242b3e2fe
Kacper Kuta <k.kuta2@student.uw.edu.pl>
2019-12-03 00:37:36 +0100

    Commit of old version - just for archivising

--- fibin.h
+++ fibin.h
@@ -5,32 +5,31 @@
 #ifndef JNP1_4_PROJECT_FIBIN_H
 #define JNP1_4_PROJECT_FIBIN_H
 
-using namespace std;
-#include<initializer_list>
+#include <unordered_map>
 
 
 template<typename T>
 class Fibin {
 public:
 
-    template<T v, typename = void>
+    template<T v>
     struct Fib {
-        const static T val = Fib<v-1>::val + Fib<v-2>::val;
+        constexpr static T val = Fib<v-1>::val + Fib<v-2>::val;
     };
 
-    template<typename F>
-    struct Fib<0, F> {
-        const static T val = 0;
+    template<>
+    struct Fib<0> {
+        constexpr static T val = 0;
     };
 
-    template<typename F>
-    struct Fib<1, F> {
-        const static T val = 1;
+    template<>
+    struct Fib<1> {
+        constexpr static T val = 1;
     };
 
     template<class Fib>
     struct Lit {
-        const static T val = Fib::val;
+        constexpr static T val = Fib::val;
     };
 
     template<class C>
@@ -38,21 +37,44 @@ public:
         std::cout << C::val;
     }
 
-
     template<typename ... Args>
     struct Sum {
-         static const T val = (... + Args::val);
+        constexpr static T val = (... + Args::val);
     };
 
     template<typename Arg>
     struct Inc1 {
-        static const T val = Arg::val + Fib<1>::val;
+        constexpr static T val = Arg::val + Fib<1>::val;
     };
 
     template<typename Arg>
     struct Inc10 {
-        static const T val = Arg::val + Fib<10>::val;
+        constexpr static T val = Arg::val + Fib<10>::val;
     };
+
+    template<class Lit1, class Lit2>
+    struct Eq {
+        constexpr static bool val = (Lit1::val == Lit2::val);
+    };
+
+    template<class Cond, class Then, class Else>
+    struct If {
+        constexpr static T val = Cond::val ? Then::val : Else::val;
+    };
+
+    std::string Var(const char* s) {
+        return std::string(s);
+    }
+
+
+    template<std::string&& v>
+    struct Ref {
+        T val = refs[v];
+    };
+
+private:
+    static std::unordered_map<std::string, T> refs;
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main() {
     using f1 = Fibin<int>::Lit<Fibin<int>::Fib<1>>;
     using f2 = Fibin<int>::Lit<Fibin<int>::Fib<4>>;
     using f3 = Fibin<int>::Lit<Fibin<int>::Fib<10>>;
+    using True = Fibin<int>::Lit<True>;
 
-
-    std::cout << Fibin<int>::Inc10<f3>::val;
+    //static_assert(Fibin<int>::Ref<>);
 }
 
 
